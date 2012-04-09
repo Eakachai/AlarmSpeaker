ab5e1ea499b8e79e60d2fe14062d0c073813d33c
Eakachai Sae-Tang <eakachai.c@gmail.com>
2012-04-10 01:08:13 +0700

    first commit

---
+++ src/com/eakachai/alarmspeaker/AlarmSpeakerActivity.java
@@ -0,0 +1,13 @@
+package com.eakachai.alarmspeaker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AlarmSpeakerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
